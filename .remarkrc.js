90a63e2ee05596a244f8658b90f1f1f89960108f
drewbourne <drew@firstbourne.com>
2018-08-23 20:34:18 -0400

    tools: Configure remarkrc, add lint-md script

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,7 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.settings = {};
 
 exports.plugins = [
-  require('remark-preset-lint-recommended'),
-  require('remark-preset-lint-consistent'),
-  require('remark-preset-lint-markdown-style-guide'),
+  require('remark-frontmatter'),
+  [
+    require('remark-retext'),
+    require('unified')().use({
+      plugins: [
+        require('retext-english'),
+        require('retext-syntax-urls'),
+        [
+          require('retext-spell'),
+          {
+            ignoreLiteral: true,
+            dictionary: require('dictionary-en-us'),
+            personal: fs.readFileSync(
+              path.join(__dirname, '.dictionary'),
+              'utf8'
+            ),
+          },
+        ],
+        [require('retext-sentence-spacing'), { preferred: 1 }],
+        require('retext-repeated-words'),
+        require('retext-usage'),
+        require('retext-indefinite-article'),
+        require('retext-redundant-acronyms'),
+        [
+          require('retext-contractions'),
+          { straight: true, allowLiteral: true },
+        ],
+        require('retext-diacritics'),
+        [require('retext-quotes'), { preferred: 'straight' }],
+      ],
+    }),
+  ],
 ];
--- vsc-extension-quickstart.md
+++ vsc-extension-quickstart.md
@@ -5,7 +5,7 @@
 - This folder contains all of the files necessary for your extension.
 - `package.json` - this is the manifest file in which you declare your extension and command.
   The sample plugin registers a command and defines its title and command name. With this information
-  VS Code can show the command in the command palette. It doesn’t yet need to load the plugin.
+  VS Code can show the command in the command palette. It doesn't yet need to load the plugin.
 - `src/extension.ts` - this is the main file where you will provide the implementation of your command.
   The file exports one function, `activate`, which is called the very first time your extension is
   activated (in this case by executing the command). Inside the `activate` function we call `registerCommand`.
